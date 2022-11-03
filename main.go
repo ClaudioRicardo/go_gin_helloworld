b27214d52edd3aee207c5fbc022cd8ec57fd72e8
ClaudioRicardo <claudwiolf@hotmail.com>
2022-11-02 22:42:42 -0300

    Atualização teste hello world

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func main() {
-
-	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "helloworld",
-		})
+func IndexHandler(c *gin.Context) {
+	name := c.Params.ByName("name")
+	c.JSON(200, gin.H{
+		"message": "hello " + name,
 	})
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/:name", IndexHandler)
 	router.Run()
 }
